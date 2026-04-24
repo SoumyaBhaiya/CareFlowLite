31969345f905ff71f8eeb59dea6015b76a48a235
SoumyaBhaiya <soumybhaiya@gmail.com>
2026-04-24 02:07:51 -0700

    getting started with Incident controller.cs

---
+++ Controllers/IncidentController.cs
@@ -0,0 +1,10 @@
+using CareFlowLite.Data;
+using CareFlowLite.DTOs;
+using CareFlowLite.Enums;
+using CareFlowLite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CareFlowLite.Controllers;
+
