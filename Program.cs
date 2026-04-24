cecaacb6ce25d9be9b3264241c30617b3e5b95c3
SoumyaBhaiya <soumybhaiya@gmail.com>
2026-04-24 13:47:11 -0700

    bug fixed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 using CareFlowLite.Data;
 using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Controllers 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//DB
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
